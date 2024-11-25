4daa8ea8169247ce2009fd1b81f958b7cdb122c8
kkm5291 <kkm23125291@gmail.com>
2024-11-25 18:06:12 +0900

    연재 4 진행중

--- spring-batch/src/main/java/com/self/batchsample/SpringBatchApplication.java
+++ spring-batch/src/main/java/com/self/batchsample/SpringBatchApplication.java
@@ -6,6 +6,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SpringBatchApplication {
 
+    /**
+     * 기본적으로 afterPropertySet(), Job, Step, Tasklet 순으로 실행?
+     * @param args
+     */
     public static void main(String[] args) {
         SpringApplication.run(SpringBatchApplication.class, args);
     }
